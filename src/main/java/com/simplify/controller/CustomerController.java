0b0ee63e6196322c36ccfd0230dba9f7500e8911
464822136 <464822136@qq.com>
2019-12-20 10:25:44 +0800

    update customerController

--- src/main/java/com/simplify/controller/CustomerController.java
+++ src/main/java/com/simplify/controller/CustomerController.java
@@ -1,13 +1,13 @@
 package com.simplify.controller;
 
+import com.simplify.model.entity.ConvertCustomer;
 import com.simplify.model.entity.Customer;
+import com.simplify.model.entity.User;
 import com.simplify.service.CustomerService;
 import com.simplify.service.LinkmanService;
+import com.simplify.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -25,10 +25,25 @@ import java.util.List;
 public class CustomerController {
     @Resource
     CustomerService customerService;
+    @Resource
+    UserService userService;
 
     @GetMapping("/list")
     @ResponseBody
     public List<Customer> listCustomer() {
         return customerService.listCustomerAndLinkman();
     }
+
+    @GetMapping("/listConvertUser")
+    @ResponseBody
+    public List<User> listConvertUser() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/saveConvert")
+    @ResponseBody
+    public int saveConvert(@RequestBody ConvertCustomer convertCustomer) {
+        return 0;
+    }
 }
+
--- src/test/java/com/simplify/persuasiblecrm/PersuasibleCrmApplicationTests.java
+++ src/test/java/com/simplify/persuasiblecrm/PersuasibleCrmApplicationTests.java
@@ -1,12 +1,9 @@
 package com.simplify.persuasiblecrm;
 
 import com.simplify.model.dto.UserAuthorizeDTO;
-<<<<<<< HEAD
 
-=======
 import com.simplify.mapper.UserMapper;
 import com.simplify.model.entity.User;
->>>>>>> 2801f2f3c835a6d70e41db2876e5185f9f78bbf8
 import com.simplify.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
