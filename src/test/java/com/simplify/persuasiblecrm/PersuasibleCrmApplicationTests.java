5ab6cc773447e7ad71b5cd9edd23a25c2107382f
杨泓 <weiji0728@163.com>
2019-12-24 08:33:19 +0800

    '.'

--- src/test/java/com/simplify/persuasiblecrm/PersuasibleCrmApplicationTests.java
+++ src/test/java/com/simplify/persuasiblecrm/PersuasibleCrmApplicationTests.java
@@ -5,11 +5,7 @@ import com.simplify.model.dto.UserAuthorizeDTO;
 import com.simplify.mapper.UserMapper;
 import com.simplify.model.entity.Business;
 import com.simplify.model.entity.User;
-<<<<<<< HEAD
 import com.simplify.service.BusinessService;
-=======
-
->>>>>>> 612c3397731ed63654cce2b98104b95fbbfbf786
 import com.simplify.service.CustomerRecordService;
 import com.simplify.service.CustomerTasksService;
 import com.simplify.service.UserService;
@@ -22,13 +18,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PersuasibleCrmApplicationTests {
 
     @Autowired
-<<<<<<< HEAD
     private CustomerRecordService customerRecordService;
 
-=======
     private CustomerTasksService customerTasksService;
->>>>>>> 612c3397731ed63654cce2b98104b95fbbfbf786
-    @Test
     void contextLoads() {
         System.out.println(customerTasksService.selectAllTasksCustomerTasks());
     }
