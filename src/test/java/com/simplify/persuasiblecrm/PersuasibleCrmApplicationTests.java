a89262a665de94b59f99edd5386f81f7a277ab47
刘俊 <demo774813973@163.com>
2019-12-24 08:45:30 +0800

    '修复Test类中BUG'

--- src/test/java/com/simplify/persuasiblecrm/PersuasibleCrmApplicationTests.java
+++ src/test/java/com/simplify/persuasiblecrm/PersuasibleCrmApplicationTests.java
@@ -5,10 +5,7 @@ import com.simplify.mapper.UserMapper;
 import com.simplify.model.entity.Business;
 import com.simplify.model.entity.User;
 import com.simplify.service.BusinessService;
-<<<<<<< HEAD
-=======
 
->>>>>>> 8d3f9ae4d6dc5e31d5bd009239adad0ecfe978d0
 import com.simplify.service.CustomerRecordService;
 import com.simplify.service.CustomerTasksService;
 import com.simplify.service.UserService;
@@ -23,13 +20,10 @@ class PersuasibleCrmApplicationTests {
     @Autowired
     private CustomerRecordService customerRecordService;
 
-<<<<<<< HEAD
     private CustomerTasksService customerTasksService;
-=======
 
-    private CustomerTasksService customerTasksService;
+
     @Test
->>>>>>> 8d3f9ae4d6dc5e31d5bd009239adad0ecfe978d0
     void contextLoads() {
         System.out.println(customerTasksService.selectAllTasksCustomerTasks());
     }
