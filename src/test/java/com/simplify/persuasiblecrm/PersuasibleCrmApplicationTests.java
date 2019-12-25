ffd9bd550e4827d624122ad52df93d946dafb10d
草泥马的小仙女 <cxxlove520@vip.qq.com>
2019-12-25 08:37:47 +0800

    线索guanli

--- src/test/java/com/simplify/persuasiblecrm/PersuasibleCrmApplicationTests.java
+++ src/test/java/com/simplify/persuasiblecrm/PersuasibleCrmApplicationTests.java
@@ -5,10 +5,6 @@ import com.simplify.mapper.UserMapper;
 import com.simplify.model.entity.Business;
 import com.simplify.model.entity.User;
 import com.simplify.service.BusinessService;
-<<<<<<< HEAD
-=======
-
->>>>>>> 8d3f9ae4d6dc5e31d5bd009239adad0ecfe978d0
 import com.simplify.service.CustomerRecordService;
 import com.simplify.service.CustomerTasksService;
 import com.simplify.service.UserService;
@@ -23,13 +19,10 @@ class PersuasibleCrmApplicationTests {
     @Autowired
     private CustomerRecordService customerRecordService;
 
-<<<<<<< HEAD
-    private CustomerTasksService customerTasksService;
-=======
 
     private CustomerTasksService customerTasksService;
+
     @Test
->>>>>>> 8d3f9ae4d6dc5e31d5bd009239adad0ecfe978d0
     void contextLoads() {
         System.out.println(customerTasksService.selectAllTasksCustomerTasks());
     }
