ba39d95f933cc384bef8a124956375e96f8edaab
杨泓 <weiji0728@163.com>
2019-12-30 09:01:45 +0800

    '更新了部分BUG'

--- src/main/java/com/simplify/mapper/BusinessMapper.java
+++ src/main/java/com/simplify/mapper/BusinessMapper.java
@@ -1,7 +1,9 @@
 package com.simplify.mapper;
 
 
+import com.simplify.model.dto.BusinessDTO;
 import com.simplify.model.entity.Business;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
@@ -16,17 +18,9 @@ import java.util.Map;
 
 @Repository
 public interface BusinessMapper extends Mapper<Business> {
+
     List<Business> selectAll();
-
-    //查询商机所有数据及实现分页功能
-    List<Business> listBusiness();
-
-    //根据id查询商机数据
-    List<Business> listBusinessById(Long id);
-<<<<<<< HEAD
-=======
-
-    //根据id删除商机数据
->>>>>>> 9f0fdb08f34d541149952e1b97044fa374f3f56c
+    List<BusinessDTO> listBusiness();
+    List<BusinessDTO> listBusinessById(Long id);
     void deleteBusinessById(Long id);
 }
