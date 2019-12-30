9357b75e67a5c273434aa6a5a7eeef4cb1d9b72d
杨泓 <weiji0728@163.com>
2019-12-30 11:41:13 +0800

    '更新了部分BUG'

--- src/main/java/com/simplify/mapper/BusinessMapper.java
+++ src/main/java/com/simplify/mapper/BusinessMapper.java
@@ -18,19 +18,10 @@ import java.util.Map;
 
 @Repository
 public interface BusinessMapper extends Mapper<Business> {
-<<<<<<< HEAD
+
 
     List<Business> selectAll();
     List<BusinessDTO> listBusiness();
     List<BusinessDTO> listBusinessById(Long id);
-=======
-
-    //查询商机所有数据及实现分页功能
-    List<Business> listBusiness();
-
-    //根据id查询商机数据
-    List<Business> listBusinessById(Long id);
-
->>>>>>> 61b4f2eb79ef8f1e927ae30fc5a88a741bc359d3
     void deleteBusinessById(Long id);
 }
